e2a4e94155ff34875be10ff165c7e67c3adcfef2
lkwan <Louie.Kwan@windriver.com>
2018-05-21 16:24:54 -0400

    v1.1

--- manifests/run-qemu-guest-agent.pp
+++ manifests/run-qemu-guest-agent.pp
@@ -2,6 +2,6 @@ file { '/tmp/qemu-agent.sh':
   ensure  => file,
   mode    => "777",
   #content => "systemctl status qemu-guest-agent",
-  content => "systemctl stop qemu-guest-agent",
+  #content => "systemctl stop qemu-guest-agent",
   #content => "systemctl start qemu-guest-agent",
 }
