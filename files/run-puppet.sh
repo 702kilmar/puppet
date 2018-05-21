26d90ed82d8c04316ee714322260fdfd7cf01e89
lkwan <Louie.Kwan@windriver.com>
2018-05-21 15:13:04 -0400

    v1.1

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /etc/puppetlabs/code/environments/production && git pull
+cd /etc/puppetlabs/code/environments/production && git pull --all && git reset --hard origin/master
 #/opt/puppetlabs/bin/puppet apply manifests/
 /usr/bin/puppet apply manifests/
